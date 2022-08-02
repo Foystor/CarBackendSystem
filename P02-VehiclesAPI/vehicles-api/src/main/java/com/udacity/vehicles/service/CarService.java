3b6a036a3dadb1dc51a46b92833e845c54901757
Foystor <foystor@gmail.com>
2022-08-03 04:53:20 +0800

    feat: Add needed web clients for CarService

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -4,6 +4,7 @@ import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * Implements the car service create, read, update or delete
@@ -14,13 +15,17 @@ import org.springframework.stereotype.Service;
 public class CarService {
 
     private final CarRepository repository;
+    private final WebClient maps;
+    private final WebClient pricing;
 
-    public CarService(CarRepository repository) {
+    public CarService(CarRepository repository, WebClient maps, WebClient pricing) {
         /**
          * TODO: Add the Maps and Pricing Web Clients you create
          *   in `VehiclesApiApplication` as arguments and set them here.
          */
         this.repository = repository;
+        this.maps = maps;
+        this.pricing = pricing;
     }
 
     /**
